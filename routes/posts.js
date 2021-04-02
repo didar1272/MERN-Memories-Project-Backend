1705f9727fa9d148fcd09b15f5b11abca89c7018
Md. Didarul Islam <reurnzero101@gmail.com>
2021-04-02 16:42:39 +0600

    Like functionality added

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getPosts, createPost, updatePost, deletePost } from "../controllers/posts.js";
+import { getPosts, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js";
 
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost); // patch is used for updating existing documents.
 router.delete('/:id', deletePost); // to delete specific post by id
+router.patch('/:id/likePost', likePost) // to like and count the number of likes in a post
 
 export default router;
